7ec3259be5e691df84c45c3a337fe2adde8bf1b6
Julian Beitzel <beitzel.julian@gmail.com>
2019-02-24 10:48:36 +0100

    Track class added to the project.

---
+++ BikeTrafficSimulator/Models/Track.cs
@@ -0,0 +1,27 @@
+﻿using GalaSoft.MvvmLight;
+using BikeTrafficSimulator.Misc;
+
+namespace BikeTrafficSimulator.Models
+{
+    public class Track : ObservableObject
+    {
+        private decimal lengthKm;
+
+        /// <summary>
+        /// Gives the length of the track in kilometers.
+        /// </summary>
+        public decimal LengthKm { get => lengthKm; set => Set(() => LengthKm, ref lengthKm, value); }
+
+        public Track(decimal lenghtKm)
+        {
+            if (lenghtKm > 0)
+            {
+                LengthKm = lenghtKm;
+            }
+            else
+            {
+                throw ExceptionCreator.GetValueAboveZeroException("Track length");
+            }
+        }
+    }
+}
