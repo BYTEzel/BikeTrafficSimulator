d9990113f9a623192854052ff641e184cefa10bc
Julian Beitzel <beitzel.julian@gmail.com>
2019-03-17 09:05:35 +0100

    Added the result view for the traffic lights

---
+++ BikeTrafficSimulator/Misc/TrafficLightStateToColor.cs
@@ -0,0 +1,23 @@
+﻿using BikeTrafficSimulator.Models;
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace BikeTrafficSimulator.Misc
+{
+    class TrafficLightStateToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var state = (TrafficLight.State)value;
+            if (TrafficLight.State.Green == state)
+                return "Green";
+            else
+                return "Red";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
